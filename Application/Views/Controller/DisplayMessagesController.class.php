80afe80427e1409c050ca3cb09b6671e257aa369
lxcool91 <715814509@qq.com>
2016-08-12 16:10:44 +0800

    functiondisplay

---
+++ Application/Views/Controller/DisplayMessagesController.class.php
@@ -0,0 +1,75 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: LX
+ * Date: 2016/8/12
+ * Time: 10:29
+ */
+
+namespace Views\Controller;
+
+use Think\Controller;
+
+class DisplayMessagesController extends Controller
+{
+
+
+//页面
+//    public function index(){
+//        $this->display();
+//    }
+
+////获取查询位置，默认为1
+//    public function id_get(){
+//        if(IS_POST){
+//            $MessageID = $_POST['id'];
+//        }else{
+//            $MessageID = 1;
+//        }
+//
+//    }
+
+//    public function connectDb(){
+//        $conn = mysqli_connect("localhost","root","Admin123");
+//        if(!$conn){
+//            echo "connect error";
+//        }else{
+//            return $conn;
+//        }
+//
+//    }
+//////获取查询数据
+////    public function data_get($conn){
+////            mysqli_select_db('db',$conn);
+//////        mysqli_query("SET NAMES 'utf-8'");
+////            $sql = "seclect * from messages";
+////            $data = mysqli_query($sql);
+////            return $data;
+////    }
+////
+//////根据ID输出查询数据
+////    public function showMessage($data,$MessageID){
+////        $count = mysqli_num_rows($data);
+////        if($MessageID>$count){
+////            echo "超过最大查询范围";
+////        }else{
+////            print_r();
+////        }
+////    }
+//
+//    public function
+
+    public function showDemo($id){
+        $data=D('DisplayMessages')->find($id);
+        $this->assign("data",$data);
+        $this->assign("id",$id);
+        $id_minus = $id-1;
+        $id_plus = $id+1;
+        $url_prev = U('Views/DisplayMessages/showDemo')."?id=$id_minus";
+        $url_next = U('Views/DisplayMessages/showDemo')."?id=$id_plus";
+        $this->assign("prev",$url_prev);
+        $this->assign("next",$url_next);
+        $this->display();
+    }
+
+}
