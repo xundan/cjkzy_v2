fb14cf855db0bf0ab7312c00bcf33b9f7e899554
xuncl <xuncl@126.com>
2016-08-16 10:38:44 +0800

    fix some bugs of DisplayMessages
    fix DM,RV by lx

--- Application/Views/Controller/DisplayMessagesController.class.php
+++ Application/Views/Controller/DisplayMessagesController.class.php
@@ -13,52 +13,6 @@ use Think\Controller;
 class DisplayMessagesController extends Controller
 {
 
-
-//页面
-//    public function index(){
-//        $this->display();
-//    }
-
-////获取查询位置，默认为1
-//    public function id_get(){
-//        if(IS_POST){
-//            $MessageID = $_POST['id'];
-//        }else{
-//            $MessageID = 1;
-//        }
-//
-//    }
-
-//    public function connectDb(){
-//        $conn = mysqli_connect("localhost","root","Admin123");
-//        if(!$conn){
-//            echo "connect error";
-//        }else{
-//            return $conn;
-//        }
-//
-//    }
-//////获取查询数据
-////    public function data_get($conn){
-////            mysqli_select_db('db',$conn);
-//////        mysqli_query("SET NAMES 'utf-8'");
-////            $sql = "seclect * from messages";
-////            $data = mysqli_query($sql);
-////            return $data;
-////    }
-////
-//////根据ID输出查询数据
-////    public function showMessage($data,$MessageID){
-////        $count = mysqli_num_rows($data);
-////        if($MessageID>$count){
-////            echo "超过最大查询范围";
-////        }else{
-////            print_r();
-////        }
-////    }
-//
-//    public function
-
     public function showDemo($id){
         // 有where包装的时候不要直接find($id)，否则where会失效
         $data=D('DisplayMessages')->where("invalid_id=0 AND status=0 AND id=".$id)->find();
@@ -66,7 +20,7 @@ class DisplayMessagesController extends Controller
         $this->assign("id",$id);
         $id_minus = $this->find_prev($id);
         $id_plus = $this->find_next($id);
-        if($data){
+        if(!$data){
             echo "<br><h1>没有更多数据了。</h1>";
         }
 
@@ -87,13 +41,15 @@ class DisplayMessagesController extends Controller
     }
 
     public function check($id){
-//        echo 'checked';
-//        $this->showDemo(0);
+
         $tags = I('post.tag');
-        $tags=I('post.id');
+
         foreach ($tags as $tag){
             echo '<br>'.$tag;
         }
+
+
+
     }
 
     public function delete($id){
@@ -116,7 +72,6 @@ class DisplayMessagesController extends Controller
         }else{
             return -1;
         }
-        return $id - 1;
     }
 
     public function find_next($id)
